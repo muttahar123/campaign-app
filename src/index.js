c957f8dd5a1c67c1f5e12e0764e498a3a5ce50cc
Muttahar Sheikh <itxmuttahar@gmail.com>
2026-04-02 09:59:22 +0500

    chore: update application port to 4000 and database mapping to port 5433

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Something broke in the server!' });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
